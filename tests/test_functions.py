d0d81d756a9e00030b4b2f3eb61630e5f2302fde
Sam Sims <samsimss98@gmail.com>
2025-10-01 16:08:11 +0100

    tests: add example test to get CI to pass

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -6,3 +6,7 @@ tests should be run before and after any changes are made.
 import pytest  # noqa: F401
 
 from mscape_template import mscape_functions as mf  # noqa: F401
+
+
+def test_example():
+    assert True
