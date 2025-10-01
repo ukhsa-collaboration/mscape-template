ec0140af11c56e48c24f7dd532f42209b34e44d4
Sam Sims <samsimss98@gmail.com>
2025-10-01 16:05:49 +0100

    tests: restore example imports

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -2,3 +2,7 @@
 Create unit tests for modules in the tests/ folder. All functions in a repo should be unit tested and
 tests should be run before and after any changes are made.
 """
+
+import pytest  # noqa: F401
+
+from mscape_template import mscape_functions as mf  # noqa: F401
